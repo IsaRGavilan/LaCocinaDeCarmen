92687ddd114bc540452ba0a8d856a2b9315d25fa
IsaRGavilan <isargavilan98@gmail.com>
2023-06-05 12:59:30 +0200

    Comentarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+/*Algunas importaciones de estilos que trae Ionic por defecto*/
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+import './theme/variables.css';
+
 import { Route } from 'react-router-dom';
 import { IonApp, IonAvatar, IonContent, IonIcon, IonImg, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
@@ -12,24 +25,8 @@ import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import { useEffect, useState } from 'react';
 import { home, heart, search, cart, person } from 'ionicons/icons';
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
 
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
 
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import './theme/variables.css';
 import './App.css';
 import PlatosPrincipales from './pages/Categorias/PlatosPrincipales';
 import Entrantes from './pages/Categorias/Entrantes';
