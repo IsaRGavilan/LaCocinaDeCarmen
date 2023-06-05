ff13071046dddea4ed5ffb0b11fd919636503bd9
IsaRGavilan <isargavilan98@gmail.com>
2023-06-05 10:10:35 +0200

    Comentarios SplashScreen

--- src/components/SplashScreen/SplashScreen.tsx
+++ src/components/SplashScreen/SplashScreen.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from 'react';
-import './SplashScreen.css'; 
+import './SplashScreen.css'; //Importa archivo de estilos
 
 const SplashScreen = () => {
   useEffect(() => {
-    // Simula un tiempo de carga de 4 segundos
+    //Simula un tiempo de carga de 4 segundos
     setTimeout(() => {
     }, 4000);
   }, []);
 
   return (
-    <div className="splash-screen">
-    </div>
+    <div className="splash-screen"/>
   );
 };
 
