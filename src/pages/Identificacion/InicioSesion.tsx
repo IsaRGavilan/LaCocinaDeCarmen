e744971e5f591f4b8b8a671e9a8488f657143dbc
IsaRGavilan <isargavilan98@gmail.com>
2023-06-05 11:54:37 +0200

    Comentarios de inicio sesion, registro y términos

--- src/pages/Identificacion/InicioSesion.tsx
+++ src/pages/Identificacion/InicioSesion.tsx
@@ -1,84 +1,92 @@
-import { IonContent, IonHeader, IonPage, IonInput, IonButton, IonToast } from '@ionic/react';
-import './InicioSesion.css';
-import google from '../../assets/imagenesGenerales/google.png';
-import { useHistory } from 'react-router-dom';
-import { useState } from 'react';
-import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import firebaseConfig from '../../firebaseConfig';
-const { app } = firebaseConfig;
+import { useState } from 'react'; //Importa useState para manejar el estado local del componente
+import { useHistory } from 'react-router-dom'; //Importa useHistory para acceder al historial de navegación
+import { IonContent, IonPage, IonInput, IonButton, IonToast } from '@ionic/react'; //Importa componentes Ionic
+import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'; //Importa funciones de la autenticación de Firebase
+import firebaseConfig from '../../firebaseConfig'; //Importa la configuración de Firebase
+import './InicioSesion.css'; //Importa archivo de estilos
+const { app } = firebaseConfig; //Coge la propiedad "app" de la configuración de Firebase
 
+//InicioSesionProps para controlar si está autenticado o no el usuario
 type InicioSesionProps = {
   setIsAuthenticated: (value: boolean) => void;
 };
 
+//Componente InicioSesion con la propiedad setIsAuthenticated
 const InicioSesion: React.FC<InicioSesionProps> = ({ setIsAuthenticated }) => {
-  const history = useHistory();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [showToast, setShowToast] = useState(false);
+  const history = useHistory(); //Hook para redirigir a otras rutas
+  const [email, setEmail] = useState(''); //Definir y almacenar email del usuario
+  const [password, setPassword] = useState(''); //Definir y almacenar contraseña del usuario
+  const [showToast, setShowToast] = useState(false); //Mostrar mensajes de comprobación o errores
   const [toastMessage, setToastMessage] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); //Controlar estado de carga del componente mientras se inicia sesión
+  //Logo para iniciar sesión con Google
   const logoGoogle = "https://firebasestorage.googleapis.com/v0/b/lacocinadecarmen-irg.appspot.com/o/fondos%2FIdentificaci%C3%B3n%20y%20T%C3%A9rminos%2Fgoogle.png?alt=media&token=9c974a95-4ad4-48ac-9a8b-401a72f362fe&_gl=1*y3wrp4*_ga*NTE5ODQ3NjM2LjE2ODQ2NzkwMTU.*_ga_CW55HF8NVT*MTY4NTg5OTY4OC4zNS4xLjE2ODU5MDE1NzMuMC4wLjA.";
 
+  //Función para redirigir a la página de registro
   const handleRegistro = () => {
     history.push('/registro');
   };
 
   const handleInicioSesion = async () => {
-    setIsLoading(true); // Habilitar el estado de carga
+    setIsLoading(true); //Mostrar indicador de carga mientras se inicia sesión
     try {
-      const auth = getAuth(app);
+      const auth = getAuth(app); //Obtiene instancia del objeto auth
+      //Uso de la función signInWithEmailAndPassword para iniciar sesión con firebase
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      const userId = userCredential.user.uid;
-      setIsAuthenticated(true); // Actualizar el estado de autenticación
-      setToastMessage('Inicio de sesión correcto');
-      console.log(userId);
-      
-      history.push("/");
+      const userId = userCredential.user.uid; //Se extrae identificador del usuario (uid) para usarlo después
+      setIsAuthenticated(true); //Actualizar el estado de autenticación
+      setToastMessage('Inicio de sesión correcto'); //Muestra mensaje de confirmación
+      history.push("/"); //Redirige al componente principal
     } catch (error) {
-      setToastMessage('Credenciales incorrectas');
+      setToastMessage('Credenciales incorrectas'); //Muestra mensaje de error
     } finally {
-      setIsLoading(false); // Deshabilitar el estado de carga
-      setShowToast(true);
+      setIsLoading(false); //Deshabilitar estado de carga
+      setShowToast(true); //Muestra el mensaje correspondiente
     }
   };
 
+  //Función para cambiar contraseña
   const handleResetPassword = async () => {
-    setIsLoading(true);
+    setIsLoading(true); //Habilitar estado de carga
     try {
-      const auth = getAuth(app);
-      await sendPasswordResetEmail(auth, email);
-      setToastMessage('Se ha enviado un correo de restablecimiento de contraseña.');
+      const auth = getAuth(app); //Obtiene instancia del objeto auth
+      await sendPasswordResetEmail(auth, email); //Uso de la función sendPasswordResetEmail de firebase
+      setToastMessage('Se ha enviado un correo de restablecimiento de contraseña.'); //Mensaje de confirmación
     } catch (error) {
+      //Mensaje de error
       setToastMessage('No se pudo enviar el correo de restablecimiento de contraseña. Verifique el correo electrónico.');
     } finally {
-      setIsLoading(false);
-      setShowToast(true);
+      setIsLoading(false); //Desactiva el estado de carga
+      setShowToast(true); //Muestra el mensaje correspondiente
     }
   };
 
+  //Función para iniciar sesión con Google
   const handleInicioSesionGoogle = async () => {
-    setIsLoading(true);
+    setIsLoading(true); //Habilita estado de carga
     try {
-      const auth = getAuth(app);
+      const auth = getAuth(app); //Obtiene instancia del objeto auth
+      //Crea instancia del proveedor de autenticación de Google con la función GoogleAuthProvider de firebase
       const provider = new GoogleAuthProvider();
+      //Utiliza la función signInWithPopup del objeto auth para mostrar ventana emergente de inicio de sesión con Google
       await signInWithPopup(auth, provider);
-      setIsAuthenticated(true);
-      setToastMessage('Inicio de sesión con Google exitoso');
-      history.push("/");
+      setIsAuthenticated(true); //Establece estado de autenticación en true
+      setToastMessage('Inicio de sesión con Google exitoso'); //Muestra mensaje de confirmación
+      history.push("/"); //Redirige al componente principal
     } catch (error) {
-      setToastMessage('Error al iniciar sesión con Google');
+      setToastMessage('Error al iniciar sesión con Google'); //Muestra mensaje de error
     } finally {
-      setIsLoading(false);
-      setShowToast(true);
+      setIsLoading(false); //Desactiva el estado de carga
+      setShowToast(true); //Muestra el mensaje correspondiente
     }
   };
+
   return (
     <IonPage>
-      <IonHeader></IonHeader>
       <IonContent fullscreen>
-        <div className="inicio-container">
-          <h1 className="titulo">¡Bienvenid@ de nuevo! Inicia sesión para continuar</h1>
+        <div className="inicio-container">{/*Contenedor principal*/}
+          <h1 className="titulo">¡Bienvenid@ de nuevo! Inicia sesión para continuar</h1>{/*Título de bienvenida*/}
+          {/*Campo de entrada para el correo electrónico*/}
           <IonInput
             label="Email"
             labelPlacement="floating"
@@ -87,7 +95,8 @@ const InicioSesion: React.FC<InicioSesionProps> = ({ setIsAuthenticated }) => {
             id="inputs"
             value={email}
             onIonChange={(e) => setEmail(e.detail.value!)}
-          ></IonInput>
+          />
+          {/*Cmapo de entrada para la contraseña*/}
           <IonInput
             label="Contraseña"
             labelPlacement="floating"
@@ -97,20 +106,22 @@ const InicioSesion: React.FC<InicioSesionProps> = ({ setIsAuthenticated }) => {
             id="inputs"
             value={password}
             onIonChange={(e) => setPassword(e.detail.value!)}
-          ></IonInput>
+          />
+          {/*Botón para iniciar sesión*/}
           <IonButton expand="full" className="boton" onClick={handleInicioSesion} disabled={isLoading}>
-          {isLoading ? 'Iniciando sesión...' : 'Iniciar sesión'}
+          {isLoading ? 'Iniciando sesión...' : 'Iniciar sesión'}{/*Texto que cambia según el estado de carga*/}
           </IonButton>
-          <h3 id="texto">¿Aún no tienes cuenta? ¡Regístrate!</h3>
+          <h3 id="texto">¿Aún no tienes cuenta? ¡Regístrate!</h3>{/*Texto y botón para ir al registro*/}
           <IonButton expand="full" className="boton" onClick={handleRegistro}>
             Registro
           </IonButton>
-          <img src={logoGoogle} id="google" alt="Google" onClick={handleInicioSesionGoogle}></img>
-          <IonButton expand="full" className="boton" onClick={handleResetPassword}>
+          <img src={logoGoogle} id="google" alt="Google" onClick={handleInicioSesionGoogle}/>{/*Imagen clicable para iniciar sesión con Google*/}
+          <IonButton expand="full" className="boton" onClick={handleResetPassword}>{/*Botón para restablecer contraseña*/}
             He olvidado la contraseña
           </IonButton>
         </div>
       </IonContent>
+      {/*Componente de notificación para mostrar los mensajes durante 3 segundos*/}
       <IonToast
         isOpen={showToast}
         onDidDismiss={() => setShowToast(false)}
--- src/pages/Identificacion/Registro.tsx
+++ src/pages/Identificacion/Registro.tsx
@@ -1,12 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonInput, IonButton, IonCheckbox, IonToast, IonLabel } from '@ionic/react';
-import './Registro.css';
-import google from '../../assets/imagenesGenerales/google.png';
-import { Link, useHistory } from 'react-router-dom';
-import { useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import firebaseConfig from '../../firebaseConfig';
-import { doc, setDoc } from 'firebase/firestore';
-const { app, firestore } = firebaseConfig;
+import { useState } from 'react'; //Importa useState para manejar el estado local del componente
+import { Link, useHistory } from 'react-router-dom'; //Link para crear enlaces a otras rutas dentro de la app e useHistory para acceder a historial navegación
+import { IonContent, IonPage, IonInput, IonButton, IonCheckbox, IonToast, IonLabel } from '@ionic/react'; //Importa componentes Ionic
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'; //Importa funciones de registro de Firebase
+import firebaseConfig from '../../firebaseConfig'; //Importa la configuración de Firebase
+import { doc, setDoc } from 'firebase/firestore'; //Importa funciones para manipular documentos de firestore
+const { app, firestore } = firebaseConfig; //Coge las propiedades app y firestore de la configuración de Firebase
+import './Registro.css'; //Importa archivo de estilos
 
 type RegistroProps = {
   setIsAuthenticated: (value: boolean) => void;
@@ -14,26 +13,29 @@ type RegistroProps = {
 
 const Registro: React.FC<RegistroProps> = ({ setIsAuthenticated }) => {
 
-  const history = useHistory();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [termsAccepted, setTermsAccepted] = useState<boolean>(false);
-  const [showToast, setShowToast] = useState(false);
+  const history = useHistory(); //Hook para redirigir a otras rutas
+  const [email, setEmail] = useState(''); //Definir y almacenar email del usuario
+  const [password, setPassword] = useState(''); //Definir y almacenar contraseña del usuario
+  const [username, setUsername] = useState(''); //Definir y almacenar username del usuario
+  const [confirmPassword, setConfirmPassword] = useState(''); //Definir y almacenar confirmación de contraseña del usuario
+  const [termsAccepted, setTermsAccepted] = useState<boolean>(false); //Definir y almacenar estado del checkbox
+  const [showToast, setShowToast] = useState(false); //Mostrar mensajes de comprobación o errores
   const [toastMessage, setToastMessage] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const [isLoading, setIsLoading] = useState(false); //Controlar estado de carga del componente mientras se realiza el registro
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //Controlar que el email introducido sea válido
+  //Logo para registrarse con Google
   const logoGoogle = "https://firebasestorage.googleapis.com/v0/b/lacocinadecarmen-irg.appspot.com/o/fondos%2FIdentificaci%C3%B3n%20y%20T%C3%A9rminos%2Fgoogle.png?alt=media&token=9c974a95-4ad4-48ac-9a8b-401a72f362fe&_gl=1*y3wrp4*_ga*NTE5ODQ3NjM2LjE2ODQ2NzkwMTU.*_ga_CW55HF8NVT*MTY4NTg5OTY4OC4zNS4xLjE2ODU5MDE1NzMuMC4wLjA.";
   
+  //Función para redirigir a la página de inicio sesión
   const handleInicioSesion = () => {
-    history.push('/inicioSesion'); // Utiliza la función push para navegar a la ruta '/inicioSesion'
-};
+    history.push('/inicioSesion');
+  }
 
+  //Función para realizar el registro
   const handleRegistro = async () => {
-    setIsLoading(true); // Habilitar el estado de carga
+    setIsLoading(true); //Habilitar el estado de carga
 
+    //Validaciones de campos
     if (!emailRegex.test(email)) {
       setIsLoading(false);
       setToastMessage('El correo electrónico no es válido. Asegúrese de ingresar un correo electrónico válido.');
@@ -62,15 +64,15 @@ const Registro: React.FC<RegistroProps> = ({ setIsAuthenticated }) => {
     }
 
     try {
-      const auth = getAuth(app);
+      const auth = getAuth(app); //Obtiene instancia del objeto auth
+      //Crear el usuario con correo electrónico y contraseña
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      const userId = userCredential.user.uid; //Obtenemos el id del usuario generado automáticamente
-      console.log(userId);
-      setIsAuthenticated(true);
-      setToastMessage('Registro correcto');
-      history.push("/");
+      const userId = userCredential.user.uid; //Se extrae identificador del usuario (uid) para usarlo después
+      setIsAuthenticated(true); //Actualizar el estado de autenticación
+      setToastMessage('Registro correcto'); //Muestra mensaje de confirmación
+      history.push("/"); //Redirige al componente principal
 
-    // Crear el documento en la colección "users" con el ID del usuario
+    //Crear el documento en la colección "users" con el ID del usuario
     await setDoc(doc(firestore, 'users', userId), {
       email: userCredential.user.email,
       username: username,
@@ -82,24 +84,27 @@ const Registro: React.FC<RegistroProps> = ({ setIsAuthenticated }) => {
       lista: [],
     });
     } catch (error) {
-      setToastMessage('El correo electrónico ya está registrado.');
+      setToastMessage('El correo electrónico ya está registrado.'); //Muestra mensaje de error
     }finally{
       setIsLoading(false); // Deshabilitar el estado de carga
-      setShowToast(true);
+      setShowToast(true); //Muestra el mensaje correspondiente
     }
   };
   
+  //Función para registrar con Google
   const handleRegistroGoogle = async () => {
-    setIsLoading(true);
+    setIsLoading(true); //Habilita estado de carga
     try {
-      const auth = getAuth(app);
+      const auth = getAuth(app); //Obtiene instancia del objeto auth
+      //Crea instancia del proveedor de autenticación de Google con la función GoogleAuthProvider de firebase
       const provider = new GoogleAuthProvider();
+      //Utiliza la función signInWithPopup del objeto auth para mostrar ventana emergente de registro con Google
       const userCredential = await signInWithPopup(auth, provider);
       const userId = userCredential.user.uid;
-      // Obtener el nombre de usuario del usuario de Google
+      //Obtener el nombre de usuario del usuario de Google
       const username = userCredential.user.displayName;
 
-      // Crear el documento en la colección "users" con el ID del usuario
+      //Crear el documento en la colección "users" con el ID del usuario
       await setDoc(doc(firestore, 'users', userId), {
         email: userCredential.user.email,
         username: username,
@@ -110,31 +115,34 @@ const Registro: React.FC<RegistroProps> = ({ setIsAuthenticated }) => {
         favoriteRecipes: [],
       });
 
-      setIsAuthenticated(true);
-      setToastMessage('Registro con Google exitoso.');
-      history.push("/");
+      setIsAuthenticated(true); //Establece estado de autenticación en true
+      setToastMessage('Registro con Google exitoso.'); //Muestra mensaje de confirmación
+      history.push("/"); //Redirige al componente principal
     } catch (error) {
-      setToastMessage('Error al registrar con Google.');
+      setToastMessage('Error al registrar con Google.'); //Muestra mensaje de error
     } finally {
-      setIsLoading(false);
-      setShowToast(true);
+      setIsLoading(false); //Desactiva el estado de carga
+      setShowToast(true); //Muestra el mensaje correspondiente
     }
   };
   
   return (
     <IonPage>
-      <IonHeader>
-      </IonHeader>
       <IonContent fullscreen>
-        <div className="registro-container">
+        <div className="registro-container"> {/*Contenedor principal*/}
           <h1 className='titulo'>Regístrate en la aplicación para disfrutar de todas las recetas que puedas imaginar...</h1>
+          {/*Campo de entrar para correo electrónico*/}
           <IonInput label="Email" labelPlacement="floating" fill="outline" placeholder="Inserte correo electrónico" id='inputs' value={email}
-            onIonChange={(e) => setEmail(e.detail.value!)}></IonInput>
+            onIonChange={(e) => setEmail(e.detail.value!)}/>
+          {/*Campo de entrara para nombre de usuario*/}
           <IonInput label="Nombre de usuario" labelPlacement="floating" fill="outline" placeholder="Inserte su nombre de usuario" id='inputs' value={username}
-            onIonChange={(e) => setUsername(e.detail.value!)}></IonInput>
+            onIonChange={(e) => setUsername(e.detail.value!)}/>
+          {/*Campo de entrada para contraseña*/}
           <IonInput label="Contraseña" labelPlacement="floating" fill="outline" placeholder="Inserte su contraseña" type='password' id='inputs' value={password}
-            onIonChange={(e) => setPassword(e.detail.value!)}></IonInput>
+            onIonChange={(e) => setPassword(e.detail.value!)}/>
+          {/*Campo de entrada para comprobación de contraseña*/}
           <IonInput label="Comprobación contraseña" labelPlacement="floating" fill="outline" placeholder="Inserte su contraseña nuevamente" type='password' id='inputs' onIonChange={(e) => setConfirmPassword(e.detail.value!)}></IonInput>
+          {/*Checkbox para aceptar términos*/}
           <IonLabel className='terms'>
           <Link to='/terminos' className='linkTerms'><span>Acepto los términos y condiciones de uso.</span></Link>
           <IonCheckbox
@@ -144,12 +152,14 @@ const Registro: React.FC<RegistroProps> = ({ setIsAuthenticated }) => {
             slot='end'
           />
         </IonLabel>
+          {/*Botón para iniciar sesión con texto que cambia según el estado de carga*/}
           <IonButton expand="full" className="boton" onClick={handleRegistro} disabled={isLoading}>{isLoading ? 'Registrando...' : 'Registrarse'}</IonButton>
-          <h3 id='texto'>¿Ya tienes cuenta? ¡Inicia sesión!</h3>
+          <h3 id='texto'>¿Ya tienes cuenta? ¡Inicia sesión!</h3>{/*Texto y botón para ir al inicio de sesión*/}
           <IonButton expand="full" className="boton" onClick={handleInicioSesion}>Iniciar sesión</IonButton>
           <img src={logoGoogle} id="google" alt="Google" onClick={handleRegistroGoogle}></img>
         </div>
       </IonContent>
+      {/*Componente de notificación para mostrar los mensajes durante 3 segundos*/}
       <IonToast
         isOpen={showToast}
         onDidDismiss={() => setShowToast(false)}
