868e2ad26a0224ebe4125749a8075b4705829dfe
IsaRGavilan <isargavilan98@gmail.com>
2023-06-05 09:20:32 +0200

    Comentarios de ProtectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,26 +1,24 @@
-import { Route, Redirect, RouteProps } from 'react-router-dom';
-import React, { ComponentType } from 'react';
+import { Route, Redirect, RouteProps } from 'react-router-dom'; //Importaciones de componentes necesarios de la librería react-router-dom
+import React, { ComponentType } from 'react'; //Importación módulo React y ComponentType para especificar el componente que se espera
 
-
+/*Define un tipo ProtectedRouteProps que extiende de RouteProps para agregar 
+la propiedad isAuthenticated y verificar que cuando el usuario acceda a una ruta
+envuelta en esto esté autenticado*/
 type ProtectedRouteProps = {
-    component: ComponentType<any>;
-    isAuthenticated: boolean;
-  } & RouteProps;
-  
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component, isAuthenticated, ...rest }) => {
-  console.log('ProtectedRoute - isAuthenticated:', isAuthenticated);
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        if (isAuthenticated) {
-          return <Component {...props} />;
-        } else {
-          return <Redirect to="/inicioSesion" />;
-        }
-      }}
-    />
-  );
-};
+  component: ComponentType<any>;
+  isAuthenticated: boolean;
+} & RouteProps;
 
-export default ProtectedRoute;
+/*Declaración del componente con las propiedades de ProtectedRouteProps*/
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component, isAuthenticated, ...rest }) => (
+  //Renderiza Route con las propiedades restantes y una función de renderizado
+  <Route
+    {...rest}
+    render={(props) =>
+      //Verifica si el usuario está autenticado para renderizar el componente, si no lo está redirige a /inicioSesion
+      isAuthenticated ? <Component {...props} /> : <Redirect to="/inicioSesion" />
+    }
+  />
+);
+
+export default ProtectedRoute; //Exportación del componente
