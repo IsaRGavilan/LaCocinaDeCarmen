5e06fee50cd9a9a38342ac4f2254440e059e7353
IsaRGavilan <isargavilan98@gmail.com>
2023-06-11 22:31:59 +0200

    Arreglo de checkbox

--- src/pages/PaginasPrincipales/Lista.tsx
+++ src/pages/PaginasPrincipales/Lista.tsx
@@ -5,12 +5,12 @@ import firebaseConfig from '../../firebaseConfig'; //Importa la configuración d
 import { downloadOutline, trashOutline } from 'ionicons/icons'; //Importa iconos utilizados
 import { getAuth } from 'firebase/auth'; //Importa función de autenticación de Firebase
 import { doc, getDoc, setDoc, onSnapshot, arrayUnion, updateDoc } from 'firebase/firestore'; //Importa funciones para manipular documentos de firestore
-import PDFDocument from 'pdf-lib/cjs/api/PDFDocument'; //Importa función para crear y manipular documentos PDF
-import { rgb } from 'pdf-lib';  //Función para crear un objeto de color en el formato RGB 
+import { PDFDocument, rgb } from 'pdf-lib';
 import '../../css/cssGenerales/Lista.css'; //Importa archivo de estilos
+import { Browser } from '@capacitor/browser';
 
 const Lista = () => {
-  const [items, setItems] = useState<{ name: string, quantity: number }[]>([]); //Almacena un array de elementos (productos) y actualiza su estado
+  const [items, setItems] = useState<{ name: string, quantity: number, checked: boolean }[]>([]); //Almacena un array de elementos (productos) y actualiza su estado
   const [currentItem, setCurrentItem] = useState<string>(''); //Almacena el valor actual del input para añadir productos a la lista
   const auth = getAuth(firebaseConfig.app); //Obtiene la instancia de autenticación de Firebase
   const user = auth.currentUser; //Obtiene el usuario autenticado actualmente
@@ -63,15 +63,17 @@ const Lista = () => {
   //Función para añadir un item a la lista
   const handleAddItem = async () => {
     if (currentItem.trim() !== '') {
-      const newItem = { name: currentItem, quantity: 1 }; //Crear el objeto del nuevo elemento
+      const newItem = { name: currentItem, quantity: 1, checked: false }; //Crear el objeto del nuevo elemento
       const updatedItems = [...items, newItem]; //Agregar el nuevo elemento a la matriz
       setItems(updatedItems); //Actualiza el estado de los elementos (productos)
       setCurrentItem(''); //Restablece el valor del input
   
       if (user) {
         try {
-          //Actualizar la lista en Firestore utilizando arrayUnion para agregar el nuevo elemento
-          await updateDoc(doc(firebaseConfig.firestore, 'users', user.uid), { lista: arrayUnion(newItem) });
+            //Actualizar la lista en Firestore utilizando arrayUnion para agregar el nuevo elemento
+            await updateDoc(doc(firebaseConfig.firestore, 'users', user.uid), {
+            lista: arrayUnion(newItem),
+          });
         } catch (error) {
           console.error('Error al actualizar la lista de la compra:', error);
         }
@@ -108,13 +110,13 @@ const Lista = () => {
         console.error('Error al actualizar la lista de la compra:', error);
       }
     }
-  };  
+  };
   
   //Función para decrementar la cantidad
   const handleDecreaseQuantity = async (index: number) => {
     const updatedItems = [...items];
     if (updatedItems[index].quantity > 1) {
-      updatedItems[index].quantity -= 1;  //Decrementa la cantidad del elemento en el índice dado
+      updatedItems[index].quantity -= 1; //Decrementa la cantidad del elemento en el índice dado
       setItems(updatedItems); //Actualiza el estado de los elementos (productos)
   
       if (user) {
@@ -126,38 +128,52 @@ const Lista = () => {
         }
       }
     }
-  };   
+  };  
 
-  //Función para descargar la lista en PDF
+  const handleCheckboxChange = async (index: number) => {
+    const updatedItems = [...items];
+    updatedItems[index].checked = !updatedItems[index].checked;
+    setItems(updatedItems);
+  
+    if (user) {
+      try {
+        const userDocRef = doc(firebaseConfig.firestore, 'users', user.uid);
+        await updateDoc(userDocRef, { lista: updatedItems });
+      } catch (error) {
+        console.error('Error al actualizar la lista de la compra:', error);
+      }
+    }
+  };
+
   const handleDownloadPDF = async () => {
     try {
       const pdfDoc = await PDFDocument.create();
       const page = pdfDoc.addPage();
       const { width, height } = page.getSize();
-
-      let y = height - 50; //Posición inicial del texto en la página PDF
-
+  
+      let y = height - 50; // Posición inicial del texto en la página PDF
+  
       for (const item of items) {
         const formattedItem = `${item.name} - Cantidad: ${item.quantity}`;
         page.drawText(formattedItem, { x: 50, y, size: 12, color: rgb(0, 0, 0) });
-        y -= 20; //Actualiza la posición Y para el siguiente elemento
+        y -= 20; // Actualiza la posición Y para el siguiente elemento
       }
-
+  
       const pdfBytes = await pdfDoc.save();
-      const blob = new Blob([pdfBytes], { type: 'application/pdf' }); // Crear un blob con los bytes del PDF
-      const url = URL.createObjectURL(blob); //Crea un objeto URL a partir del blob
-      const a = document.createElement('a'); // Crear un elemento <a> y simular el clic para iniciar la descarga
-
-      a.href = url; //Establece el atributo "href" con el URL
-      a.download = 'Mi lista de la compra.pdf'; //Establece el atributo "download" con el nombre Mi lista de la compra
-      a.click(); // Simula el clic en el enlace para iniciar la descarga del PDF
-
-      URL.revokeObjectURL(url); //Libera el URL del objeto Blob
+  
+      // Crear un blob con los bytes del PDF
+      const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+  
+      // Crear una URL a partir del blob
+      const url = URL.createObjectURL(blob);
+  
+      // Abrir el PDF en una nueva ventana utilizando el plugin @capacitor/browser
+      await Browser.open({ url });
     } catch (error) {
       console.error('Error al generar el PDF:', error);
     }
   };
-
+  
   return (
     <IonPage id="main-content" className="main-page">
       <IonHeader className="custom-header"> {/*Header del componente que incluye el menú desplegable*/}
@@ -183,7 +199,7 @@ const Lista = () => {
         <IonList id='lista'>{/*Lista que muestra un mapa de los elementos añadidos*/}
           {items.map((item, index) => (
             <IonItem key={index} id='custom-item'>
-              <IonCheckbox slot="start" value={item.name} checked={false} aria-label={item.name} className="custom-checkbox" color={'secondary'}/>
+              <IonCheckbox slot="start" value={item.name} checked={item.checked} onIonChange={() => handleCheckboxChange(index)}  aria-label={item.name} className="custom-checkbox" color={'secondary'} />
               <IonLabel id='custom-label'>{item.name}</IonLabel>
               <IonButton onClick={() => handleDecreaseQuantity(index)} className='botonCantidad'>-</IonButton> {/*Botón de disminuir cantidad */}
               <span>{item.quantity}</span> {/*Mostrar la cantidad*/}
